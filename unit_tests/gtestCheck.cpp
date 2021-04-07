be8b363fb9ed31575ba77055091790b7957d40ce
mdus <marcin.dus@nokia.com>
2021-04-07 14:25:18 +0200

    added gtest to build system

---
+++ unit_tests/gtestCheck.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+TEST(SimpleTest, CompareEqualValues)
+{
+    ASSERT_EQ(1, 1);
+}
