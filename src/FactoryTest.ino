cb094e70a2ea2a0f5ccfa23a29f5f68407aa63b8
duszek <marcin.dus@gmail.com>
2021-04-08 22:00:56 +0200

    another experiment wit sniffer see https://blog.podkalicki.com/esp32-wifi-sniffer/

--- src/FactoryTest.ino
+++ src/FactoryTest.ino
@@ -20,6 +20,18 @@
 #include "WiFi.h"
 #include "Animation.h"
 #include "Geometry.h"
+#include "Arduino.h"
+
+
+#include "freertos/FreeRTOS.h"
+#include "esp_wifi.h"
+#include "esp_wifi_types.h"
+#include "esp_system.h"
+#include "esp_event.h"
+#include "esp_event_loop.h"
+#include "nvs_flash.h"
+#include "driver/gpio.h"
+
 
 const unsigned char* Animationptr[18] = {
   stick1,stick2,stick3,stick4,stick5,
@@ -27,6 +39,100 @@ const unsigned char* Animationptr[18] = {
   stick11,stick12,stick13,stick14,stick15,
   stick16,stick17,stick18
 };
+
+#define WIFI_CHANNEL_SWITCH_INTERVAL  (500)
+#define WIFI_CHANNEL_MAX               (13)
+
+uint8_t level = 0, channel = 1;
+
+static wifi_country_t wifi_country = {.cc="CN", .schan = 1, .nchan = 13}; //Most recent esp32 library struct
+
+typedef struct {
+  unsigned frame_ctrl:16;
+  unsigned duration_id:16;
+  uint8_t addr1[6]; /* receiver address */
+  uint8_t addr2[6]; /* sender address */
+  uint8_t addr3[6]; /* filtering address */
+  unsigned sequence_ctrl:16;
+  uint8_t addr4[6]; /* optional */
+} wifi_ieee80211_mac_hdr_t;
+
+typedef struct {
+  wifi_ieee80211_mac_hdr_t hdr;
+  uint8_t payload[0]; /* network data ended with 4 bytes csum (CRC32) */
+} wifi_ieee80211_packet_t;
+
+static esp_err_t event_handler(void *ctx, system_event_t *event);
+static void wifi_sniffer_init(void);
+static void wifi_sniffer_set_channel(uint8_t channel);
+static const char *wifi_sniffer_packet_type2str(wifi_promiscuous_pkt_type_t type);
+static void wifi_sniffer_packet_handler(void *buff, wifi_promiscuous_pkt_type_t type);
+
+esp_err_t event_handler(void *ctx, system_event_t *event)
+{
+  return ESP_OK;
+}
+
+void wifi_sniffer_init(void)
+{
+  nvs_flash_init();
+  tcpip_adapter_init();
+  ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
+  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+  ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
+  ESP_ERROR_CHECK( esp_wifi_set_country(&wifi_country) ); /* set country for channel range [1, 13] */
+  ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
+  ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_NULL) );
+  ESP_ERROR_CHECK( esp_wifi_start() );
+  esp_wifi_set_promiscuous(true);
+  esp_wifi_set_promiscuous_rx_cb(&wifi_sniffer_packet_handler);
+}
+
+void wifi_sniffer_set_channel(uint8_t channel)
+{
+  esp_wifi_set_channel(channel, WIFI_SECOND_CHAN_NONE);
+}
+
+const char * wifi_sniffer_packet_type2str(wifi_promiscuous_pkt_type_t type)
+{
+  switch(type) {
+  case WIFI_PKT_MGMT: return "MGMT";
+  case WIFI_PKT_DATA: return "DATA";
+  default:  
+  case WIFI_PKT_MISC: return "MISC";
+  }
+}
+
+void wifi_sniffer_packet_handler(void* buff, wifi_promiscuous_pkt_type_t type)
+{
+  if (type != WIFI_PKT_MGMT)
+    return;
+
+  const wifi_promiscuous_pkt_t *ppkt = (wifi_promiscuous_pkt_t *)buff;
+  const wifi_ieee80211_packet_t *ipkt = (wifi_ieee80211_packet_t *)ppkt->payload;
+  const wifi_ieee80211_mac_hdr_t *hdr = &ipkt->hdr;
+
+  printf("PACKET TYPE=%s, CHAN=%02d, RSSI=%02d,"
+    " ADDR1=%02x:%02x:%02x:%02x:%02x:%02x,"
+    " ADDR2=%02x:%02x:%02x:%02x:%02x:%02x,"
+    " ADDR3=%02x:%02x:%02x:%02x:%02x:%02x\n",
+    wifi_sniffer_packet_type2str(type),
+    ppkt->rx_ctrl.channel,
+    ppkt->rx_ctrl.rssi,
+    /* ADDR1 */
+    hdr->addr1[0],hdr->addr1[1],hdr->addr1[2],
+    hdr->addr1[3],hdr->addr1[4],hdr->addr1[5],
+    /* ADDR2 */
+    hdr->addr2[0],hdr->addr2[1],hdr->addr2[2],
+    hdr->addr2[3],hdr->addr2[4],hdr->addr2[5],
+    /* ADDR3 */
+    hdr->addr3[0],hdr->addr3[1],hdr->addr3[2],
+    hdr->addr3[3],hdr->addr3[4],hdr->addr3[5]
+  );
+}
+
+
+
 
 
 #define NUM_LEDS 3
@@ -807,6 +913,22 @@ void DisplayMicro()
 }
 void MyDisplay()
 {
+
+  Serial.begin(115200);
+  delay(10);
+
+  wifi_sniffer_init();
+
+  //Serial.print("inside loop");
+  delay(1000); // wait for a second
+  vTaskDelay(WIFI_CHANNEL_SWITCH_INTERVAL / portTICK_PERIOD_MS);
+  wifi_sniffer_set_channel(channel);
+  channel = (channel % WIFI_CHANNEL_MAX) + 1;
+
+
+
+
+
     Disbuff.fillRect(0, 0, 160, 80, Disbuff.color565(0, 0, 0));
     Displaybuff();
     while ((!M5.BtnA.isPressed()) && (!M5.BtnB.isPressed()))
